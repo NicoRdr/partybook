bfb37c5e385880edd1556a164a527537c367dea3
NicoR <nicolas.rodrigues94@gmail.com>
2015-11-20 12:12:25 +0100

    ajout controller soiree modifier et crée un new pour créer des soirée

--- app/controllers/soiree_controller.rb
+++ app/controllers/soiree_controller.rb
@@ -6,20 +6,20 @@ class SoireeController < ApplicationController
   end
 
   def create
-    @soiree = Soiree.create(soiree_params)
+     @soiree = Soiree.create(params[:soiree])
   end
   
 
  
 
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_soiree
-  #     @soiree= Soiree.find(params[:id])
-  #   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_soiree
+      @soiree = Soiree.find(params[:id])
+    end
 
-  #   # Never trust parameters from the scary internet, only allow the white list through.
-  #   def soiree_params
-  #     params.require(:soiree).permit(:user_id, :soiree_id, :create, :destroy, :show)
-  #   end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def soiree_params
+      params.require(:soiree).permit(:user_id, :soiree_id, :create, :destroy, :show)
+    end
 end
